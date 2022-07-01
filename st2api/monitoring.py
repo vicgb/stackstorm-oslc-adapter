009c4b58be403c1ddf58cf918c0885f817d173c1
valvarez <victor.alvarez@upm.es>
2022-07-01 15:16:00 +0200

    Remove innecesary files and change env vars

--- st2api/monitoring.py
+++ st2api/monitoring.py
@@ -11,34 +11,12 @@ token = os.getenv("TOKEN")
 
 headers = {'X-Auth-Token': token}
 
-def get_database():
-    
-
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb://fd6bd9c7f88c:27017"
- 
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    
-    client = pymongo.MongoClient(CONNECTION_STRING, replicaset='rs')
-    
-    # Create the database for our example (we will use the same database throughout the tutorial
-    
-    return client
-
-def get_mongo_rules():
-    
-    dbname = get_database()
-    rule_collection = dbname["rule_d_b"]
-    
-    
-
-    return rule_collection
-
 
 def stream_mongo():
 
-    client = pymongo.MongoClient("mongodb://fd6bd9c7f88c:27017", replicaset='rs')
-    headers = headers
+    client = pymongo.MongoClient("mongodb://"+mongo_url, replicaset=mongo_replicaset)
+
+
     # db: st2
     # collection: rule_d_b 
     change_stream = client.st2.rule_d_b.watch()
@@ -46,7 +24,7 @@ def stream_mongo():
     for change in change_stream:
         
         requests.post("http://localhost:5000/service/ST2Logs", headers=headers, data=dumps(change))
-        print(change)
+      
 
 stream_mongo()
 
